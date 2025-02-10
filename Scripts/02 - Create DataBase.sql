fad61988d711007852885d8f272ba4e3e53fe9a0
gchapani <gleison.chapani@terceiroslinx.com.br>
2025-02-10 18:33:24 -0300

    Script and documentation

---
+++ Scripts/02 - Create DataBase.sql
@@ -0,0 +1,14 @@
+-- Database: DeveloperEvaluation
+
+-- DROP DATABASE IF EXISTS "DeveloperEvaluation";
+
+CREATE DATABASE "DeveloperEvaluation"
+    WITH
+    OWNER = sa
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
